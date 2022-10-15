1e0b515e7651522f5289aa34056894900e9440ff
moc <1287607885@qq.com>
2022-10-15 21:48:34 +0800

    多通道统计RGB值，修改为正确的函数名

--- GreyValueStaticsWith3Channel.py
+++ GreyValueStaticsWith3Channel.py
@@ -6,7 +6,7 @@ img = cv2.imread('Photos/sekiro.jpg')
 color = {'b', 'g', 'r'}  # 在不同的通道上进行统计
 
 for i, col in enumerate(color):
-    histr = cv2.clacHist([img], [i], None, [256], [0, 256])
+    histr = cv2.calcHist([img], [i], None, [256], [0, 256])
     plt.plot(histr, color=col)
     plt.xlim([0, 256])
 
