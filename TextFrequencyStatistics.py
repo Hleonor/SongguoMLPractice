a3781be65053b7a24b39584b74f934dae5570847
moc <1287607885@qq.com>
2022-10-15 22:24:34 +0800

    统计文本词频，增加一点注释

--- TextFrequencyStatistics.py
+++ TextFrequencyStatistics.py
@@ -1,24 +1,27 @@
-with open('Text/test.txt', 'r', encoding='UTF-8') as novelfile:
-    novel = novelfile.read()
+with open('Text/test.txt', 'r', encoding='UTF-8') as novelFile:
+    novel = novelFile.read()
 
 import jieba
 
-novellist = list(jieba.lcut(novel))
+novelList = list(jieba.lcut(novel))
 
-stopwords = [line.strip() for line in open('Text/stop.txt' , 'r' , encoding='UTF-8').readlines()]
+stopwords = [line.strip() for line in open('Text/stop.txt', 'r', encoding='UTF-8').readlines()]
 
-noveldict = {}
+novelDict = {}
 
-for word in novellist:
+for word in novelList:
     if word not in stopwords:
         if len(word) == 1:  # 长度为1，不进行统计
             continue
         else:
-            noveldict[word] = noveldict.get(word, 0) + 1  # 和之前的setDefault操作类似
+            novelDict[word] = novelDict.get(word, 0) + 1  # 和之前的setDefault操作类似
 
-novellistsorted = list(noveldict.items())
-novellistsorted.sort(key = lambda e:e[1] , reverse = True)
+novelListSorted = list(novelDict.items())  # 通过items()方法将字典转换为元组列表
+'''
+    通过lambda函数，将元组列表的第二个元素作为排序的关键字
+    reverse=True表示降序排列
+'''
+novelListSorted.sort(key=lambda e: e[1], reverse=True)
 
-topwordnum = 0
-for topwordup in novellistsorted[:20]:
-    print(topwordup)
+for topWordUp in novelListSorted[:20]:  # 统计词频最高的前20个词
+    print(topWordUp)
